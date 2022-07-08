9dcd34083f4b2f4753ccbb7c18ea9c2fb79da5e1
edanv <edwin.villarraga@pysltda.com>
2022-07-08 09:08:16 -0500

    FEAT: pagina de links valiosos

--- Front/src/app/component/links/links.component.ts
+++ Front/src/app/component/links/links.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-links',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./links.component.scss']
 })
 export class LinksComponent implements OnInit {
+  dataSource: Array<{}> = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
 
-  ngOnInit(): void {
+  async obtenerLinks() {
+    await this.http.get('https://artemisaback.netlify.app/.netlify/functions/api/link-valioso').toPromise().then((response: any) => {
+      if (response?.data) {
+        for (const current of response['data']) {
+          current['tags'] = current['tags'].split(',');
+        }
+        this.dataSource = response['data'];
+      }
+    });
+
+  }
+
+  async ngOnInit(): Promise<any> {
+    await Promise.resolve().then(async () => {
+      await this.obtenerLinks();
+    });
   }
 
 }
