493adaf20c7f48b1e003e0a968c48d52b3c93845
Luxuan205 <juandigonzalezvillarreal@gmail.com>
2026-04-09 01:39:28 -0500

    feat(navbar): add drawer state, FA icons, Router injection

--- ArtemisaAngular3.0/src/app/menu-bar/menu-bar.component.ts
+++ ArtemisaAngular3.0/src/app/menu-bar/menu-bar.component.ts
@@ -1,25 +1,55 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, HostListener, OnInit } from '@angular/core';
+import { RouterModule, Router } from '@angular/router';
 import { ThemeService } from '../services/theme.service';
 import { AuthService } from '../services/auth.service';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faBolt, faHouse, faBookOpen, faPuzzlePiece,
+  faLink, faCalendar, faBars, faXmark, faSun, faMoon
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-menu-bar',
-  imports: [RouterModule],
+  imports: [RouterModule, FontAwesomeModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './menu-bar.component.html',
   styleUrl: './menu-bar.component.css',
 })
 export class MenuBarComponent implements OnInit {
+  drawerOpen = false;
+
+  faBolt = faBolt;
+  faHouse = faHouse;
+  faBookOpen = faBookOpen;
+  faPuzzlePiece = faPuzzlePiece;
+  faLink = faLink;
+  faCalendar = faCalendar;
+  faBars = faBars;
+  faXmark = faXmark;
+  faSun = faSun;
+  faMoon = faMoon;
+
   constructor(
     public theme: ThemeService,
     private authService: AuthService,
+    public router: Router,
   ) {}
 
   toggleDarkMode() {
-    const darkModeContainer = document.querySelector('.darkmode-container');
-    darkModeContainer?.classList.toggle('active');
     this.theme.toggle();
+  }
+
+  toggleDrawer() {
+    this.drawerOpen = !this.drawerOpen;
+  }
+
+  closeDrawer() {
+    this.drawerOpen = false;
+  }
+
+  @HostListener('document:keydown.escape')
+  onEscape() {
+    this.drawerOpen = false;
   }
 
   ngOnInit(): void {
